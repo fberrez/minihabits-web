151a55f12c04a960a78ccaa97d7da4a47e5bdf77
fberrez <github.alphabet035@passinbox.com>
2025-01-29 20:27:41 +0100

    fix(sound): add a console.error

--- src/lib/sound.ts
+++ src/lib/sound.ts
@@ -3,7 +3,8 @@ const successSound = new Audio("../../../public/success.mp3");
 export const playSuccessSound = () => {
   successSound.currentTime = 0; // Reset the sound to start
   successSound.volume = 0.2; // Set volume to 20%
-  successSound.play().catch(() => {
-    // Ignore errors - some browsers block autoplay
+  successSound.play().catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error("Failed to play success sound", error);
   });
 };
